5d50a6e7d3dbeb0746e6b4c4ca19c400b7610758
Mister Wolfgang <mister.wolfgang.69@gmail.com>
2025-08-29 22:56:58 +0200

    Initial commit

---
+++ eslint.config.cjs
@@ -0,0 +1,23 @@
+const rnPlugin = require('@react-native-community/eslint-plugin');
+
+module.exports = [
+  {
+    ignores: ['**/node_modules/**', '**/android/**', '**/ios/**'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: { __DEV__: 'readonly' },
+    },
+  },
+  ...(rnPlugin && rnPlugin.configs && rnPlugin.configs.recommended
+    ? [rnPlugin.configs.recommended]
+    : []),
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      eqeqeq: ['error', 'always'],
+      'prefer-const': 'warn',
+    },
+  },
+];
