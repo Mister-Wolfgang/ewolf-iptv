1c86106cfd2f9b5613589953910c788cd0f12f7f
Mister Wolfgang <mister.wolfgang.69@gmail.com>
2025-08-29 23:46:01 +0200

    Adds React Native Paper UI library
    Configures Babel to support React Native Paper.
    Moves the main App component to the src directory.
    Wraps the App component with PaperProvider for theming.
    Adds reset cache to the start script.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-paper/babel',
+  ],
+  env: {
+    production: {
+      // keep other production-only plugins here if needed
+      plugins: []
+    },
+  },
 };
